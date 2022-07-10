25dc729c3b2aabdefd574b9c511ddce0bc0c4fab
pavel <pavel-babin@yandex.ru>
2022-07-10 18:54:38 +0500

    Четвертая задача

---
+++ task004/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу где на входе принимает одно число n, а на выходе похазывает все целые числа 
+//в промежутке от -n до n
+Console.WriteLine("Ведите число n: ");
+int number = int.Parse(Console.ReadLine());
+
+for (int i = -number; i <= number; i++ )
+{
+    Console.WriteLine(i);
+}
