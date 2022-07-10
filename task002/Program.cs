f47f9996f86b41234e7eae9deb99506b2b3c5a4e
pavel <pavel-babin@yandex.ru>
2022-07-10 17:36:34 +0500

    Вторая задача

---
+++ task002/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу которая на вход принимае два числа и проверяет является ли первое число корнем второго.
+Console.WriteLine("Введите первое число: ");
+int nomberA = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int nomberB = int.Parse(Console.ReadLine());
+if (nomberB == nomberA/nomberB)
+{
+    Console.WriteLine("Верно");
+}
+else
+{
+    Console.WriteLine("Лож");
+}
