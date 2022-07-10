70956ac0e837c5f50a4517baaf4d08dcef5c6a85
pavel <pavel-babin@yandex.ru>
2022-07-10 16:40:09 +0500

    Task 001

---
+++ task001/Program.cs
@@ -0,0 +1,4 @@
+﻿// Напишите программу, которая на вход принимает число и выдает его квадрат.
+Console.WriteLine("Введите число: ");
+int nam = int.Parse(Console.ReadLine());
+Console.WriteLine("Квадрат равен =  " + nam * nam);
