fe3b784352c360a7934bc53db11ab94e76f911aa
pavel <pavel-babin@yandex.ru>
2022-07-12 22:53:35 +0500

    Task 006

---
+++ task006/Program.cs
@@ -0,0 +1,16 @@
+﻿
+
+int num = new Random().Next(10, 100);
+int num2 = num % 10;
+int num1 = num / 10;
+int max = num1;
+
+Console.WriteLine($"num1 = {num1}, num2 = {num2}");
+
+if (num2 > max)
+{
+  max = num2;
+}
+
+Console.WriteLine(max);
+
