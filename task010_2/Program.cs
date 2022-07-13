de87a06827d23e5fad518dc2a3c78cd2cca521b3
pavel <pavel-babin@yandex.ru>
2022-07-13 15:07:16 +0500

    Task 010 2

---
+++ task010_2/Program.cs
@@ -0,0 +1,20 @@
+﻿// Программа которая принимает на входе два числа и проверяет является ли одно число квадратом другого
+Console.WriteLine("Введите первое число: ");
+int nomberA = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int nomberB = int.Parse(Console.ReadLine());
+
+//Можно так
+//if ((nomberA*nomberA == nomberB) | (nomberB*nomberB == nomberA))
+if (nomberA*nomberA == nomberB)
+{
+    Console.WriteLine("Является квадратом первого числа");
+}
+else if(nomberB*nomberB == nomberA)
+    {
+        Console.WriteLine("Является квадратом второго числа");
+    }
+else
+{
+    Console.WriteLine("Нет");
+}
