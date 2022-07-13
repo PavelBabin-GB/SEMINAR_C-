d9eddc5afab2f96bdddb77ca9a0d4731bf25669a
pavel <pavel-babin@yandex.ru>
2022-07-13 13:37:41 +0500

    Task 008 2

---
+++ task008_2/Program.cs
@@ -0,0 +1,15 @@
+﻿// Программа которая принимает на входе два числа и выводит, является ли второе число кратным первому
+Console.WriteLine("Введите первое число: ");
+int nomberA = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int nomberB = int.Parse(Console.ReadLine());
+
+
+if (nomberA % nomberB == 0)
+{
+    Console.WriteLine("Кратное");
+}
+else
+{
+    Console.WriteLine("Не кратное, остаток = "+ nomberA%nomberB);
+}
