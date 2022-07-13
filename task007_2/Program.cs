1bbcc23dcd1aca6bba5d9f16bce01a13aa43b6da
pavel <pavel-babin@yandex.ru>
2022-07-13 12:44:10 +0500

    Task 007 2

---
+++ task007_2/Program.cs
@@ -0,0 +1,8 @@
+﻿// Программа которая выводит случайное трехзначное число и удоляет вторую цыфру этого числа
+int num = new Random().Next(100, 1000);
+Console.WriteLine(num);
+int num1 = num/100;
+int num2 = num%10;
+//Console.WriteLine($"{num1}{num2}");
+
+Console.Write(num1*10+num2);
