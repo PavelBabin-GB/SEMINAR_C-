d3779eab66595226ec9154729e68338b882c479f
pavel <pavel-babin@yandex.ru>
2022-07-13 14:08:33 +0500

    Task 009 2

---
+++ task009_2/Program.cs
@@ -0,0 +1,12 @@
+﻿// Программа которая принимает на входе число и проверяет кратно ли оно одновременно 7 и 23
+Console.WriteLine("Введите число: ");
+int nomber = int.Parse(Console.ReadLine());
+
+if (nomber % 7 == 0 & nomber % 23 == 0)
+{
+    Console.WriteLine("Кратное числам 7 и 23");
+}
+else
+{
+    Console.WriteLine("Не кратное числам 7 и 23");
+}
