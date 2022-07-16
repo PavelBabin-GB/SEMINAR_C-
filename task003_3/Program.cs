ae5b40463944ca2f621619b97f04d6a22609eea7
pavel <pavel-babin@yandex.ru>
2022-07-16 20:42:38 +0500

    Task 003_3

---
+++ task003_3/Program.cs
@@ -0,0 +1,16 @@
+﻿// Программа котороя принимает на вход коордионаты двух точек и находит расстояние между ними в 2D пространстве 
+Console.WriteLine("Введите Xa");
+int xa = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите Ya");
+int ya = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите Xb");
+int xb = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите Yb");
+int yb = int.Parse(Console.ReadLine());
+
+double Distance (int xaLocal, int yaLocal, int xbLocal, int ybLocal)
+{
+    double dis = Math.Sqrt(Math.Pow(xbLocal - xaLocal, 2) + Math.Pow(ybLocal - yaLocal, 2));
+    return dis;
+}
+Console.WriteLine(Distance(xa, ya, xb, yb));
