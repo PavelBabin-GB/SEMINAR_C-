4c882524077ac99b6bcafad9dbdb9d8314f03b0d
pavel <pavel-babin@yandex.ru>
2022-07-16 21:16:16 +0500

    Task 004_3

---
+++ task004_3/Program.cs
@@ -0,0 +1,11 @@
+﻿// Программа которая принимает на входе число n и выдает таблицу квадратов чисел от 1 до n
+Console.WriteLine("Введите целое число");
+int number = int.Parse(Console.ReadLine());
+double Result(int iLocal)
+{
+    return Math.Pow(iLocal, 2);
+}
+for (int i = 1; i <= number; i++)
+{
+    Console.WriteLine(Result(i));
+}
