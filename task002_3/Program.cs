bb442caee5ffe632f7b19a2cc9b2ab9e77a9aeed
pavel <pavel-babin@yandex.ru>
2022-07-16 18:58:28 +0500

    Task 002_3

---
+++ task002_3/Program.cs
@@ -0,0 +1,18 @@
+﻿// Программа которая по заданному номеру четверти показывает диапозон возможных коардинат точек в этой 
+//плоскости Х и У
+string gotRange(int q)
+{
+    if (q == 1) return "x > 0, y > 0";
+    if (q == 2) return "x > 0, y < 0";
+    if (q == 3) return "x < 0, y < 0";
+    if (q == 4) return "x < 0, y > 0";
+    return "";
+}
+int q;
+do
+{
+    Console.WriteLine("Введите число: ");
+    q = int.Parse(Console.ReadLine());
+}
+while (q < 1 || q > 4);
+Console.WriteLine(gotRange(q));
