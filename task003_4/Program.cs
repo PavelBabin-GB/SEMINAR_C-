aaf08e1c2de07f8e8f6967455beb25cfaa6b7212
pavel <pavel-babin@yandex.ru>
2022-07-20 17:33:14 +0500

    Task 003_4

---
+++ task003_4/Program.cs
@@ -0,0 +1,14 @@
+﻿// Программа которая выводит массив из 8 элементов заполненный нулями и единицами в случайом порядке
+int[] GetNewArrey(int lengthArr)
+{
+    int[] arrey = new int[lengthArr];
+    for (int i = 0; i < arrey.Length; i++)
+    {
+        arrey[i] = new Random().Next(0, 2);
+    }
+    return arrey;
+}
+foreach (int item in GetNewArrey(8))
+{
+    Console.Write(item + " ");
+}
