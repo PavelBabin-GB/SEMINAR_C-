58bec4f9005301e9bc5920e435ca1d31b113f3c3
pavel <pavel-babin@yandex.ru>
2022-07-20 14:24:11 +0500

    Task 001_4

---
+++ task001_4/Program.cs
@@ -0,0 +1,15 @@
+﻿// Программа которая принимает на входе число А и выдает суму от 1 до А
+int getSumNums(int number)
+{
+    int sum = 0;
+    while (number > 0)
+    {
+        sum = sum + number;
+        number = number - 1;
+    }
+    return sum;
+} 
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+
+Console.WriteLine(getSumNums(num));
