fb43dafd68c4be411e19bfa84067bdf6f24c4b6b
pavel <pavel-babin@yandex.ru>
2022-07-20 16:17:25 +0500

    Task 002_4

---
+++ task002_4/Program.cs
@@ -0,0 +1,14 @@
+﻿// Программа которая принимает на вход число и выдает количество цыфр в числе
+int getLength(int num)
+{
+    int count = 0;
+    while (num > 0)
+    {
+        num = num / 10;
+        count++;        
+    }
+    return count;
+}
+Console.WriteLine("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine(getLength(n));
