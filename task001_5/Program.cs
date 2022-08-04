ca98dc70d08b9a5b24e8b52400793a459f93109c
pavel <pavel-babin@yandex.ru>
2022-08-04 12:37:39 +0500

    Task001 5

---
+++ task001_5/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задайте массив из 12 элементов, заполненый случайными числами из промежутка [-9, 9] 
+// наидите сумму отрицательных и положительных элементов массива.
+
+int[] arrey = GetArrey(12, -9, 9);
+
+int pos = 0;
+int neg = 0;
+
+int[] GetArrey(int size, int min, int max)
+{
+    int[] result = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(min, max + 1);
+    }
+    return result;
+}
+Console.Write("[");
+foreach (int i in arrey)
+{
+    Console.Write(i + ",");
+    pos += i > 0 ? i : 0;
+    neg += i < 0 ? i : 0;
+}
+Console.Write("]");
+Console.WriteLine();
+Console.WriteLine($"NEG = {neg} POS = {pos}");
+
+
