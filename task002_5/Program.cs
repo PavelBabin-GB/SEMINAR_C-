e0378c2d21593b09536bd823891205eb019293a1
pavel <pavel-babin@yandex.ru>
2022-08-04 14:28:48 +0500

    Task002 5

---
+++ task002_5/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу замены элементов массива: отрицательные замените на положительные и наоборот.
+
+int[] GetArrey(int size, int min, int max)
+{
+    int[] result = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(min, max + 1);
+    }
+    return result;
+}
+int[] arrey = GetArrey(12, -9, 9);
+    for (int i = 0; i < arrey.Length; i++)
+        {
+            Console.Write(arrey[i] + " ");
+        }
+    Console.WriteLine();
+    void Print (int[] ar)
+    {
+        for (int i = 0; i < ar.Length; i++)
+        {
+            ar[i] *= -1;
+            Console.Write(ar[i] + " ");
+        }
+    }
+    Print (arrey);
