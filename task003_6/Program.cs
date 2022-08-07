7259bfd32ad5d6727f5b543962b1f86c4305d4b2
pavel <pavel-babin@yandex.ru>
2022-08-07 18:40:42 +0500

    task003 6/

---
+++ task003_6/Program.cs
@@ -0,0 +1,20 @@
+﻿// функция для перевода из десятичной в любую
+
+Console.WriteLine("Введите число для перевода: ");
+int decNumber = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите функцию: ");
+int otherSustem = int.Parse(Console.ReadLine());
+
+string DecToNum(int decNumber, int otherSustem)
+{
+    string res = "";
+    string nums = "0123456789ABCDEF";
+    while(decNumber > 0)
+    {
+        int oct = decNumber / otherSustem;
+        res = nums[decNumber - otherSustem * oct] + res;
+        decNumber /= otherSustem;
+    }
+    return res;
+}
+Console.WriteLine(DecToNum(decNumber, otherSustem));
